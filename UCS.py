0f4015ba1e87144720f9e774d6de4f1cbf9f130b
192425292simats-byte <192425292.simats@saveetha.com>
2026-07-15 14:43:23 +0530

    Update UCS.py

--- UCS.py
+++ UCS.py
@@ -1,5 +1,4 @@
 from queue import PriorityQueue
-
 graph = {
     'A': [('B', 2), ('C', 4)],
     'B': [('D', 7), ('E', 3)],
@@ -9,31 +8,22 @@ graph = {
     'F': [('G', 1)],
     'G': []
 }
-
 def uniform_cost_search(graph, start, goal):
     pq = PriorityQueue()
     pq.put((0, start, [start]))
     visited = {}
-
     while not pq.empty():
         cost, node, path = pq.get()
-
         if node == goal:
             return path, cost
-
         if node not in visited or cost < visited[node]:
             visited[node] = cost
-
             for neighbour, weight in graph[node]:
                 pq.put((cost + weight, neighbour, path + [neighbour]))
-
     return None, float('inf')
-
 start = input("Enter Start Node: ")
 goal = input("Enter Goal Node: ")
-
 path, cost = uniform_cost_search(graph, start, goal)
-
 if path:
     print("\nShortest Path:", " -> ".join(path))
     print("Total Cost:", cost)
